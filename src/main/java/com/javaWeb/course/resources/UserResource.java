88c5b2dfd526d1a3b11cf10a12f628ebed93049a
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2023-02-12 00:33:49 -0300

    FindById

--- src/main/java/com/javaWeb/course/resources/UserResource.java
+++ src/main/java/com/javaWeb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,12 @@ public class UserResource {
 		List<User> listUsers = service.findAll();
 
 		return ResponseEntity.ok().body(listUsers);		
+	}
+
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User u = service.findById(id);
+		return u != null ? ResponseEntity.ok().body(u) : ResponseEntity.badRequest().body(null);
 	}
 
 	@PostMapping
--- src/main/java/com/javaWeb/course/services/UserService.java
+++ src/main/java/com/javaWeb/course/services/UserService.java
@@ -1,9 +1,10 @@
 package com.javaWeb.course.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.javaWeb.course.entities.User;
 import com.javaWeb.course.repositories.UserRepository;
 
@@ -20,5 +21,9 @@ public class UserService {
 
     public User CreateUser(User u){
         return users.save(u);
+    }
+
+    public User findById(Long id){
+        return users.findById(id).get();
     }
 }
